6f500f52ea24013477b9560d3344a08782631e02
Erick <ek110193@gmail.com>
2017-01-09 23:31:38 +0700

    Add raja ongkir config

---
+++ src/config/raja-ongkir.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'api' => 'http://api.rajaongkir.com/starter',
+    'apiKey' => 'ffd410f9fadfc91cbbd2d66c36d3fcc5',
+    'originCityId' => 151, // Jakarta Barat Based on RajaOngkir API
+    'couriers' => [
+        'jne'  => 'JNE',
+        'tiki' => 'TIKI'
+    ]
+];
